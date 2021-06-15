06c88dc0a44db8cd933d65ca6ae7d4d1dd69c67a
SpicyMeatloaf <jordan.leewc@gmail.com>
2021-06-14 18:39:51 -0700

    minor changes

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -3,6 +3,8 @@ var Review = require('../models/review');
 module.exports = {
     index,
     create,
+    update,
+    delete: deleteReview,
 }
 
 function index(req, res) {
@@ -15,4 +17,16 @@ function create(req, res) {
     Review.create(req.body, function (err, review) {
         res.status(201).json({review});
     });
+}
+
+function update(req, res) {
+    Review.findByIdAndUpdate(req.params.id, req.body, function() {
+        index(req, res);
+    })
+}
+
+function deleteReview(req, res) {
+    Review.findByIdAndDelete(req.params.id, function() {
+        index(req, res);
+    })
 }
--- routes/reviews.js
+++ routes/reviews.js
@@ -4,5 +4,7 @@ var reviewsCtrl = require('../controllers/reviews');
 
 router.get('/', reviewsCtrl.index);
 router.post('/', reviewsCtrl.create);
+router.put('/:id', reviewsCtrl.update);
+router.delete('/:id', reviewsCtrl.delete);
 
 module.exports = router;
